5075c10a28b0ca8b333021c83c119ad586463936
童建伟 <tongjiann@gmail.com>
2026-04-13 22:28:38 +0800

    chore: 删除前端相关代码

--- diboot-admin-ui/src/directives/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-import { hasRole, hasPermission } from './permission'
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export default ({ directive }: App) => {
-  directive('hasRole', hasRole)
-  directive('hasPermission', hasPermission)
-}
--- diboot-admin-ui/src/views/system/i18n-config/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts" name="I18nConfig">
-import I18nConfig from './List.vue'
-</script>
-
-<template>
-  <i18n-config />
-</template>
