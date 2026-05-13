a703ce3094eb1107c3ad8f126ce7d47323725348
童建伟 <tongjiann@gmail.com>
2026-05-13 19:43:38 +0800

    fix: 更新后刷新定时任务

--- src/main/java/com/xiw/kuwei/service/common/impl/ScheduleTaskServiceImpl.java
+++ src/main/java/com/xiw/kuwei/service/common/impl/ScheduleTaskServiceImpl.java
@@ -2,10 +2,13 @@ package com.xiw.kuwei.service.common.impl;
 
 import com.diboot.core.service.impl.BaseServiceImpl;
 import com.xiw.kuwei.entity.common.ScheduleTask;
+import com.xiw.kuwei.helper.ScheduleTaskHelper;
 import com.xiw.kuwei.mapper.common.ScheduleTaskMapper;
 import com.xiw.kuwei.service.common.ScheduleTaskService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 
 /**
@@ -18,5 +21,35 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class ScheduleTaskServiceImpl extends BaseServiceImpl<ScheduleTaskMapper, ScheduleTask> implements ScheduleTaskService {
+
+    @Override
+    protected void afterBatchCreate(Collection<ScheduleTask> entityList) {
+        ScheduleTaskHelper.refresh();
+        super.afterBatchCreate(entityList);
+    }
+
+    @Override
+    protected void afterCreate(ScheduleTask entity) {
+        ScheduleTaskHelper.refresh();
+        super.afterCreate(entity);
+    }
+
+    @Override
+    protected void afterDelete(Object entityIds) {
+        ScheduleTaskHelper.refresh();
+        super.afterDelete(entityIds);
+    }
+
+    @Override
+    protected void afterDelete(String fieldKey, Object fieldVal) {
+        ScheduleTaskHelper.refresh();
+        super.afterDelete(fieldKey, fieldVal);
+    }
+
+    @Override
+    protected void afterUpdate(ScheduleTask entity) {
+        ScheduleTaskHelper.refresh();
+        super.afterUpdate(entity);
+    }
 
 }
